548ac5bbb2143ccf12b404a0ff79ea10f4eb123f
Ronaldoh1 <2ronald.hernandez@gmail.com>
2020-03-06 21:47:47 -0500

    htmt section complete

---
+++ addImage.js
@@ -0,0 +1,11 @@
+// adding image and handing it off to the next guy.
+function  addImage(req, res, next) {
+
+  console.log("LOGIT: request %S, %S", req.method, req.url);
+
+  if(url.match())
+
+  next();
+}
+
+module.export = addImage;
